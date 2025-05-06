8c1bcacc6a6e59f00033efb1056a1839d050119f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-05-06 09:29:17 +0200

    chore: delay SpawnTraderGUI display to improve player interaction experience

--- src/main/java/dev/slne/spawn/trader/entity/EntityInteractListener.java
+++ src/main/java/dev/slne/spawn/trader/entity/EntityInteractListener.java
@@ -1,13 +1,14 @@
 package dev.slne.spawn.trader.entity;
 
+import dev.slne.spawn.trader.SpawnTrader;
 import dev.slne.spawn.trader.gui.SpawnTraderGUI;
-
 import lol.pyr.znpcsplus.api.event.NpcInteractEvent;
 import lol.pyr.znpcsplus.api.npc.NpcEntry;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class EntityInteractListener implements Listener {
 
@@ -17,7 +18,12 @@ public class EntityInteractListener implements Listener {
     Player player = event.getPlayer();
 
     if(entry.getId().equalsIgnoreCase("spawn-trader")) {
-      new SpawnTraderGUI(player).show(player);
+      new BukkitRunnable() {
+        @Override
+        public void run() {
+          new SpawnTraderGUI(player).show(player);
+        }
+      }.runTaskLater(SpawnTrader.instance(), 1L);
     }
   }
 }
