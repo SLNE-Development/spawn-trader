f82b9685c80da3310704558a1ecc33a5f941d423
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-05-07 22:42:37 +0200

    chore: update version to 1.21.4-1.1.0-SNAPSHOT and clean up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,11 @@ plugins {
 
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
     id("com.gradleup.shadow") version "8.3.0"
-    id("org.hibernate.build.maven-repo-auth") version "3.0.4"
     id ("io.freefair.lombok") version "8.10"
 }
 
 group = "dev.slne.spawn"
-version = "1.21.4-1.0.0-SNAPSHOT"
+version = "1.21.4-1.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -27,8 +26,6 @@ repositories {
     maven ("https://repo.pyr.lol/snapshots") {
         name = "pyrSnapshots"
     }
-
-
 }
 
 dependencies {
@@ -36,8 +33,7 @@ dependencies {
     compileOnly ("dev.jorel:commandapi-bukkit-core:9.7.0")
     compileOnly("lol.pyr:znpcsplus-api:2.1.0-SNAPSHOT")
 
-    implementation ("com.github.stefvanschie.inventoryframework:IF:0.10.19")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+    implementation("com.github.stefvanschie.inventoryframework:IF:0.10.19")
 }
 
 paper {
@@ -46,10 +42,10 @@ paper {
     apiVersion = "1.21"
     authors = listOf("red")
     prefix = "SpawnTrader"
-    version = "1.21.4-1.0.0-SNAPSHOT"
+    version = "1.21.4-1.1.0-SNAPSHOT"
 
 
-    serverDependencies{
+    serverDependencies {
         register("CommandAPI") {
             load = PaperPluginDescription.RelativeLoadOrder.BEFORE
             required = true
@@ -66,18 +62,11 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
-
-    withSourcesJar()
-    withJavadocJar()
 }
 
-tasks.compileJava{
+tasks.compileJava {
     options.encoding = Charsets.UTF_8.name()
     options.compilerArgs.add("-parameters")
-}
-
-tasks.javadoc {
-    options.encoding = Charsets.UTF_8.name()
 }
 
 tasks.shadowJar {
